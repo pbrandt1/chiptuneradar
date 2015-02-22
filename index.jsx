91cd1e8d32794634b247321c81e66f1bd0043435
Peter Brandt <peter.m.brandt@gmail.com>
2015-02-21 22:13:40 -0500

    oh god what have i done?

--- index.jsx
+++ index.jsx
@@ -1,4 +1,34 @@
 var React = require('react');
 var Map = require('./Map');
+var Walkway = require('walkway.js');
+var createArcadeGrid = require('./arcade-grid');
+
+skel.init();
+
+createArcadeGrid();
+
+var svg = new Walkway({
+    selector: '#arcade-grid',
+    duration: '2000',
+    // can pass in a function or a string like 'easeOutQuint'
+    easing: function (t) {
+        return t;
+    }
+});
+
+setTimeout(function() {
+    svg.draw(function() {
+        console.log('eond');
+        document.getElementById("arcade-overlay").style.height = "6em";
+        var int = setInterval(createArcadeGrid, 1000/70); // OH GOD WHAT HAVE I DONE?
+        setTimeout(function() {
+            clearInterval(int);
+        }, 2000)
+    });
+}, 1000);
+
+
+
+
 
 React.render(<Map />, document.getElementById('content'));
